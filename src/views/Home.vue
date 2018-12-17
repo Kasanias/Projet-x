fe13af6726919e3abf63e13b971612a8ce79ae1d
Esteban Casanas <esteban.ca19@gmail.com>
2018-12-17 11:35:33 +0100

    Initializing repo

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div
+      class="home-container"
+      :style="{ backgroundImage: `url('${require('@/assets/movies-background.jpg')}')` }"
+    ></div>
+    
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+.home-container {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
